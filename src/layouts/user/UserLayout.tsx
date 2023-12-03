3ab15ddf456a782471ba7b1c0c6f13af3694e6b3
might guy <onyejemeemmanuel65@gmail.com>
2023-12-03 05:45:36 +0100

    Added flutterwave refId

--- src/layouts/user/UserLayout.tsx
+++ src/layouts/user/UserLayout.tsx
@@ -19,6 +19,7 @@ export function UserLayout() {
 
     // Get Location
     const location = useLocation();
+    
     const currentLocationPathname = location.pathname;
 
     // profile image selector from the state
